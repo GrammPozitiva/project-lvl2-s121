f482c0da5fe75208dc6a7af5fad9a71baa71fdc3
GrammPozitiva <tanuxxxa_s@mail.ru>
2017-09-05 12:48:02 +0300

    added gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
-import run from '..';
 
-run();
+const program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
