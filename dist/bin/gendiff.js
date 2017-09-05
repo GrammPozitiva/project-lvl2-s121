b8fdd6cf72ea0250b3bb15f8ca951844c81477f6
GrammPozitiva <tanuxxxa_s@mail.ru>
2017-09-06 01:46:37 +0300

    fixes

--- dist/bin/gendiff.js
+++ dist/bin/gendiff.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-var _index = require('../index');
+var _referenceGendiff = require('../referenceGendiff');
 
-var _index2 = _interopRequireDefault(_index);
+var _referenceGendiff2 = _interopRequireDefault(_referenceGendiff);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _index2.default)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vZ2VuZGlmZi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUVBOzs7Ozs7QUFFQSIsImZpbGUiOiJnZW5kaWZmLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5cbmltcG9ydCBydW4gZnJvbSAnLi4vaW5kZXgnO1xuXG5ydW4oKTtcbiJdfQ==
+(0, _referenceGendiff2.default)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vZ2VuZGlmZi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUVBOzs7Ozs7QUFFQSIsImZpbGUiOiJnZW5kaWZmLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5cbmltcG9ydCBydW4gZnJvbSAnLi4vcmVmZXJlbmNlR2VuZGlmZic7XG5cbnJ1bigpO1xuIl19
